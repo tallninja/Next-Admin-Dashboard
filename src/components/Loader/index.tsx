6c58d251bd0823f7958404ed200c0b606913bab0
tallninja <ernestwambua2@gmail.com>
2023-10-08 17:13:26 +0300

    Added responsiveness to the sidebar

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Loader() {
+	return (
+		<div className='flex h-screen items-center justify-center bg-white'>
+			<div className='h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent'></div>
+		</div>
+	);
+}
